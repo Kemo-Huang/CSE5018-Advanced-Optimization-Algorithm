9202daa436a75a02602ff560fce8e3ac72bc0989
Kemo-Huang <12032943@mail.sustech.edu.cn>
2021-05-11 16:04:36 +0800

    update

--- MultiTaskOptimization/knapsack.cpp
+++ MultiTaskOptimization/knapsack.cpp
@@ -6,19 +6,56 @@ using namespace std;
 
 class Knapsack {
 public:
-    float capacity;
+    int n_items;
+    int capacity;
     vector<int> weights;
     vector<int> profits;
 
     Knapsack(int n_items, float capacity) {
-        this->capacity = capacity;
+        this->n_items = n_items;
+        this->capacity = int(capacity);
         weights = vector<int>(n_items, 0);
         profits = vector<int>(n_items, 0);
     }
 
-    void solve(){
-
+    int dp_solve() {
+        vector<vector<int>> matrix(n_items + 1, vector<int>(capacity + 1, 0));
+        int evaluations = 0;
+        for (int i = 1; i <= n_items; ++i) {
+            for (int w = 1; w <= capacity; ++w) {
+                if (weights[i - 1] > w) {
+                    matrix[i][w] = matrix[i - 1][w];
+                } else {
+                    evaluations += 1;
+                    matrix[i][w] = max(matrix[i - 1][w], profits[i - 1] + matrix[i - 1][w - weights[i - 1]]);
+                }
+            }
+        }
+        cout << "best value " << matrix[n_items][capacity] << endl;
+        return evaluations;
     }
+
+    int dp_solve_value() {
+        const int inf = 10000000;
+        const int max_value = 21300;
+        int value, evaluations = 0;
+        vector<int> matrix(max_value + 1, inf);
+        matrix[0] = 0;
+        for (int i = 0; i < n_items; ++i) {
+            for (int j = max_value; j >= profits[i]; --j){
+                evaluations += 1;
+                matrix[j] = min(matrix[j - profits[i]] + weights[i], matrix[j]);
+            }
+        }
+        for (int i =0; i < max_value; ++i){
+            if (matrix[i] < capacity) {
+                value = i;
+            }
+        }
+        cout << "best value "<< value << endl;
+        return evaluations;
+    }
+
 };
 
 
@@ -52,6 +89,11 @@ void read_knapsack_data(const string &inputFileName, vector<Knapsack> &outputKna
 int main() {
     vector<Knapsack> knapsacks;
     read_knapsack_data("../knapsack_2_500.txt", knapsacks);
-
+    for (Knapsack &knapsack : knapsacks) {
+        int evaluations = knapsack.dp_solve();
+        cout << "evaluations " << evaluations << endl;
+        evaluations = knapsack.dp_solve_value();
+        cout << "evaluations " << evaluations << endl;
+    }
     return 0;
 }
