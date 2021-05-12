70cbb86eeef6cbdd2ac9aad7edf165b7d790c332
kemo-huang <11610728@mail.sustc.edu.cn>
2021-05-12 23:10:49 +0800

    update

--- MultiTaskOptimization/main.py
+++ MultiTaskOptimization/main.py
@@ -1,14 +1,15 @@
 import numpy as np
+import tqdm
+import copy
 
 
 class Solution:
-    def __init__(self):
-        self.idx = []
+    def __init__(self, n_items):
+        self.idx = np.zeros(n_items, dtype=np.bool)
         self.weight_1 = 0
         self.value_1 = 0
         self.weight_2 = 0
         self.value_2 = 0
-        self.evaluations = 0
 
 
 class TwoKnapsacksProblem:
@@ -20,6 +21,7 @@ class TwoKnapsacksProblem:
         self.values_1 = np.zeros(self.n_items, dtype=np.int)
         self.weights_2 = np.zeros(self.n_items, dtype=np.int)
         self.values_2 = np.zeros(self.n_items, dtype=np.int)
+        self.evaluations = 0
 
         self.capacity_1 = int(float(lines[2]))
         lines = lines[3:]
@@ -38,12 +40,12 @@ class TwoKnapsacksProblem:
     def greedy(self, w1=1.0, w2=1.0):
         vow = w1 * self.vow_1 + w2 * self.vow_2
         idx = np.argsort(-vow)
-        solution = Solution()
+        solution = Solution(self.n_items)
         for i in idx:
-            solution.evaluations += 1
+            self.evaluations += 1
             if self.weights_1[i] + solution.weight_1 <= self.capacity_1 \
                     and self.weights_2[i] + solution.weight_2 <= self.capacity_2:
-                solution.idx.append(i)
+                solution.idx[i] = True
                 solution.weight_1 += self.weights_1[i]
                 solution.value_1 += self.values_1[i]
                 solution.weight_2 += self.weights_2[i]
@@ -51,6 +53,37 @@ class TwoKnapsacksProblem:
             if solution.weight_1 == self.capacity_1 or solution.weight_2 == self.capacity_2:
                 break
         return solution
+
+    def flip(self, solution, n_offsprings=100):
+        chosen_idx = np.arange(self.n_items)[solution.idx]
+        not_chosen_idx = np.arange(self.n_items)[~solution.idx]
+        paired_indices = np.array([(x, y) for x in chosen_idx for y in not_chosen_idx])
+        new_solutions = []
+        indices = np.random.choice(len(paired_indices), n_offsprings, replace=False)
+        paired_indices = paired_indices[indices]
+        self.evaluations += len(paired_indices)
+        for idx_pair in paired_indices:
+            new_solution_idx = solution.idx.copy()
+            new_solution_idx[idx_pair[0]] = False
+            new_solution_idx[idx_pair[1]] = True
+            # feasible and non-dominated
+            new_weight_1 = solution.weight_1 - self.weights_1[idx_pair[0]] + self.weights_1[idx_pair[1]]
+            if new_weight_1 < self.capacity_1:
+                new_weight_2 = solution.weight_2 - self.weights_2[idx_pair[0]] + self.weights_2[idx_pair[1]]
+                if new_weight_2 < self.capacity_2:
+                    new_value_1 = solution.value_1 - self.values_1[idx_pair[0]] + self.values_1[idx_pair[1]]
+                    new_value_2 = solution.value_2 - self.values_2[idx_pair[0]] + self.values_2[idx_pair[1]]
+                    if (solution.value_1 > new_value_1 and solution.value_2 >= new_value_2) or (
+                            solution.value_2 > new_value_2 and solution.value_1 >= new_value_1):
+                        continue
+                    new_solution = Solution(self.n_items)
+                    new_solution.idx = new_solution_idx
+                    new_solution.weight_1 = new_weight_1
+                    new_solution.weight_2 = new_weight_2
+                    new_solution.value_1 = new_value_1
+                    new_solution.value_2 = new_value_2
+                    new_solutions.append(new_solution)
+        return new_solutions
 
 
 def non_dominated_solutions(solutions):
@@ -73,26 +106,71 @@ def non_dominated_solutions(solutions):
                 dominated_idx.append(i)
                 break
     non_dominated_idx = [i for i in range(len(solutions)) if i not in dominated_idx]
-    print(non_dominated_idx)
+    # print(non_dominated_idx)
     return solutions[non_dominated_idx]
 
 
 def main():
     problem = TwoKnapsacksProblem()
-    solutions = []
-    for i in range(10):
+    initial_solutions = []
+    for i in range(5):
         solution = problem.greedy(1.0 + 0.1 * i, 1.0)
         # print(solution.value_1, solution.value_2, solution.evaluations)
-        solutions.append(solution)
+        initial_solutions.append(solution)
     # print('-----------------')
-    for i in range(1, 10, 1):
+    for i in range(1, 5, 1):
         solution = problem.greedy(1.0, 1.0 + 0.1 * i)
         # print(solution.value_1, solution.value_2, solution.evaluations)
-        solutions.append(solution)
-    solutions.append(problem.greedy(sum(problem.vow_1) / problem.n_items, sum(problem.vow_2) / problem.n_items))
-    solutions = np.array(solutions)
-    solutions = non_dominated_solutions(solutions)
-    print([(i.value_1, i.value_2) for i in solutions])
+        initial_solutions.append(solution)
+    initial_solutions = np.array(initial_solutions)
+    initial_solutions = non_dominated_solutions(initial_solutions)
+
+    evaluations = problem.evaluations
+    all_solutions = []
+    all_evaluations = []
+    for _ in tqdm.tqdm(range(5)):
+        solutions = copy.deepcopy(initial_solutions)
+        problem.evaluations = evaluations
+        while len(solutions) < 100:
+            new_solutions = []
+            for s in solutions:
+                new_solutions += problem.flip(s)
+            solutions = np.concatenate([np.array(new_solutions), solutions])
+            solutions = non_dominated_solutions(solutions)
+
+        all_solutions.append([(solution.value_1, solution.value_2) for solution in solutions[:100]])
+        # print(problem.evaluations)
+        all_evaluations.append(problem.evaluations)
+
+    all_area = []
+    for i in range(len(all_solutions)):
+        solutions = all_solutions[i]
+        solutions = sorted(solutions, key=lambda tup: tup[0])
+        all_solutions[i] = solutions
+
+        area = 0
+        tmp = 0
+        for value_1, value_2 in solutions:
+            area += (value_1 - tmp) * value_2
+            tmp = value_1
+        all_area.append(area)
+
+    all_area = np.array(all_area, dtype=np.float64)
+    avg_area = np.sum(all_area) / len(all_area)
+    print('avg area:', avg_area)
+
+    max_idx = np.argmax(all_area)
+    print('max area:', all_area[max_idx])
+
+    all_evaluations = np.array(all_evaluations, dtype=np.float64)
+    avg_evaluations = np.sum(all_evaluations) / len(all_evaluations)
+    print('avg evaluations:', avg_evaluations)
+
+    best_solutions = all_solutions[max_idx]
+
+    with open('solution.csv', 'w+') as f:
+        for value_1, value_2 in best_solutions:
+            f.write(str(value_1) + ',' + str(value_2) + '\n')
 
 
 if __name__ == '__main__':
